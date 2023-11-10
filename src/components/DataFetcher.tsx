388b0e91e9e462be2e5d8e0e7a8f38cacaee0485
dltlaos11 <dltlaos14@naver.com>
2023-11-10 23:59:19 +0900

    feat: 의존성 배열을 통해 Paginagtion 적용 & useQueryClient를 통한 프리패칭

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { DataFetcherDetail } from "./DataFetcherDetail";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import {ReactQueryDevtools} from "react-query/devtools"
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum: number) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 const DataFetcher = () => {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const {data, isError,error , isLoading} = useQuery("posts", fetchPosts, {staleTime: 2000}) as {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if(currentPage < maxPostPage) { // 9페이지 이전이라면 프리페칭이 일어나지만, 10페이지일 경우 미리 가져올 데이터가 없음
+        const nextPage = currentPage + 1;
+        queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+        // 9페이지라면 10패이지의 데이터가 미리 다운
+    }
+  }, [currentPage, queryClient])
+
+  const {data, isError,error , isLoading} = useQuery(["posts", currentPage], () => fetchPosts(currentPage), 
+  {
+    staleTime: 2000,
+    keepPreviousData: true // 쿼리 키가 바뀔 떄도 지난 데이터를 유지해서 이전 페이지로 돌아갔을 떄 캐시에 해당 데이터가 존재하도록 하기위함
+    // 이전의 쿼리에 대한 반환값이 캐시에 남아 있음
+}) as {
     data: any; // Specify the type for 'data'
     isError: boolean;
     error: Error | null; // Specify the type for 'error'
@@ -47,11 +62,17 @@ const DataFetcher = () => {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1}
+         onClick={() => {
+            setCurrentPage((previousValue) => previousValue -1)
+         }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage}
+         onClick={() => {
+            setCurrentPage((previousValue) => previousValue +1) 
+         }}>
           Next page
         </button>
       </div>
